c1eb34195e68c46fb00d0d114bf2ddc44bd8b1b6
Kavishka Thushal <128395749+KavishkaThushal@users.noreply.github.com>
2024-05-18 21:42:13 +0530

    toggle navbar done

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import Logo from '../../assets/logo.png'
 function Navbar() {
   const [menu,setMenu] = useState('')
+  const [menuShow,setMenuShow] = useState(false)
   return (
    <div className='nav-bar'>
     <img src={Logo} alt='logo' className='logo' />
@@ -23,8 +24,21 @@ function Navbar() {
      <FiSearch  /> 
      <FiShoppingCart  /> 
      <button className='sign-btn'>SIGN IN</button>
-     <RxHamburgerMenu className='menu-icon'/>
+     <RxHamburgerMenu className='menu-icon' onClick={()=>(setMenuShow(!menuShow))}/>
     </div>
+
+    {menuShow? <div className='toggle-navbar'>
+    <ul className='toggle-nav-menu'>
+      
+      <li onClick={()=>(setMenu('home'))} className={menu==='home'? 'active':''}>HOME</li>
+      <li onClick={()=>(setMenu('menu'))} className={menu==='menu'? 'active':''}>MENU</li>
+      <li onClick={()=>(setMenu('mobile-app'))} className={menu==='mobile-app'? 'active':''}>MOBILE-APP</li>
+      <li onClick={()=>(setMenu('contact'))} className={menu==='contact'? 'active':''}>CONTACT US</li>
+    
+  </ul>
+  <button className='toggle-sign-btn'>SIGN IN</button>
+
+    </div>:''}
    </div>
   )
 }
