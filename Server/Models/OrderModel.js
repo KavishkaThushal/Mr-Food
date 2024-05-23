a186c76cea70be322e7cec91c64921e68dcb0892
Kavishka Thushal <128395749+KavishkaThushal@users.noreply.github.com>
2024-05-23 17:53:56 +0530

    create ordermodel

---
+++ Server/Models/OrderModel.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const orderSchema=new mongoose.Schema({
+    userId:{
+        type:String,
+        required:true
+    },
+    items:{
+        type:Array,
+        required:true
+    },
+    amount:{
+        type:Number,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    status:{
+        type:String,
+        default:"pending"
+    },
+    date:{
+        type:Date,
+        default:Date.now()
+    },
+    payment:{
+        type:Boolean,
+        default:false
+    }
+})
+
+const Order=mongoose.model('Order',orderSchema);
+export default Order;
